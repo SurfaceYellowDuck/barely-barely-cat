de5d0174589f40e60024ca0b69b643fe6be4b7ac
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-01-11 22:32:47 +0300

    feat: partitial compile support in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,13 @@ plugins {
     kotlin("jvm")
     id("org.jetbrains.compose")
     id("org.jetbrains.kotlin.plugin.compose")
+    id ("org.graalvm.buildtools.native") version "0.9.8"
 }
+
+//val launcher = javaToolchains.launcherFor {
+//    languageVersion = JavaLanguageVersion.of(21)
+//    vendor = JvmVendorSpec.ORACLE
+//}
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -21,6 +27,7 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
 //    implementation (com.google)
+//    implementation(oracle)
     implementation ("com.google.code.gson:gson:2.10.1")
     implementation(compose.desktop.currentOs)
     testImplementation(kotlin("test"))
@@ -40,4 +47,17 @@ compose.desktop {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+graalvmNative {
+    binaries {
+        named("main") {
+            imageName.set("application")
+            mainClass.set("MainKt")
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+                vendor.set(JvmVendorSpec.matching("GraalVM Community"))
+            })
+        }
+    }
 }

34e4b3106148a9f15685b785efcb6383ff5a45e5
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-01-23 02:21:53 +0300

    janitorial: some refactored CatStates

--- src/main/kotlin/CatStates.kt
+++ src/main/kotlin/CatStates.kt
@@ -2,24 +2,18 @@ import kotlin.random.Random
 import androidx.compose.ui.graphics.Color
 
 interface CatState {
-    fun nextMove(): Pair<Float, Float>
+    fun nextMove(): Pair<Float, Float> {
+        return Random.nextFloat() * 2 - 1 to Random.nextFloat() * 2 - 1
+    }
     val color: Color
 }
 
 class WalkingState : CatState {
-    override fun nextMove(): Pair<Float, Float> {
-        return Random.nextFloat() * 2 - 1 to Random.nextFloat() * 2 - 1
-    }
-
     override val color: Color
         get() = Color.Green
 }
 
 class FightingState : CatState {
-    override fun nextMove(): Pair<Float, Float> {
-        return Random.nextFloat() * 2 - 1 to Random.nextFloat() * 2 - 1
-    }
-
     override val color: Color
         get() = Color.Red
 }
@@ -34,10 +28,6 @@ class SleepingState : CatState {
 }
 
 class HissingState : CatState {
-    override fun nextMove(): Pair<Float, Float> {
-        return Random.nextFloat() * 2 - 1 to Random.nextFloat() * 2 - 1
-    }
-
     override val color: Color
         get() = Color.Yellow
 }
